ba35f260c8a8834c7e7d1f200ec89b248268c464
Khaki <ponpontanuki16@gmail.com>
2024-06-30 00:07:35 +0900

    生成モデルを作成するヘルパーのビルダークラスを作成

---
+++ app/src/main/java/com/example/cookingidea/model/GeminiModelHelper.kt
@@ -0,0 +1,57 @@
+package com.example.cookingidea.model
+
+import androidx.annotation.ReturnThis
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.generationConfig
+
+object GeminiModelHelper {
+
+    enum class ModelType(val modelName: String) {
+        FLASH("gemini-1.5-flash"),
+        PRO("gemini-1.5-pro"),
+    }
+
+    enum class ResponseType(val responseMimeType: String) {
+        TEXT("text/plain"),
+        JSON("application/json"),
+    }
+
+    class Build {
+        private var modelName: String = ModelType.FLASH.modelName
+        private var apiKey: String? = null
+        private var responseType: ResponseType = ResponseType.TEXT
+
+        @ReturnThis
+        fun setModel(modelType: ModelType): Build {
+            modelName = modelType.modelName
+            return this
+        }
+
+        @ReturnThis
+        fun setApiKey(apiKey: String): Build {
+            this.apiKey = apiKey
+            return this
+        }
+
+        @ReturnThis
+        fun setResponseType(responseType: ResponseType): Build {
+            this.responseType = responseType
+            return this
+        }
+
+        fun build(): GenerativeModel {
+            if (apiKey == null) throw IllegalArgumentException("apiKey is required")
+            return GenerativeModel(
+                modelName,
+                apiKey!!,
+                generationConfig = generationConfig {
+                    temperature = 1f
+                    topK = 64
+                    topP = 0.95f
+                    maxOutputTokens = 8192
+                    responseMimeType = responseType.responseMimeType
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/cookingidea/ui/screen/main/MainViewModel.kt
+++ app/src/main/java/com/example/cookingidea/ui/screen/main/MainViewModel.kt
@@ -5,10 +5,9 @@ import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.cookingidea.BuildConfig
+import com.example.cookingidea.model.GeminiModelHelper
 import com.example.cookingidea.ui.screen.menudialog.MenuDialogUiState
-import com.google.ai.client.generativeai.GenerativeModel
 import com.google.ai.client.generativeai.type.content
-import com.google.ai.client.generativeai.type.generationConfig
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -29,19 +28,11 @@ class MainViewModel : ViewModel() {
     private val _menuDialogUiStateFlow = MutableStateFlow(MenuDialogUiState())
     val menuDialogUiStateFlow: StateFlow<MenuDialogUiState> = _menuDialogUiStateFlow.asStateFlow()
 
-    private val model = GenerativeModel(
-        "gemini-1.5-flash",
-        BuildConfig.gemini_api_key,
-        generationConfig = generationConfig {
-            temperature = 1f
-            topK = 64
-            topP = 0.95f
-            maxOutputTokens = 8192
-            responseMimeType = "text/plain"
-        },
-        // safetySettings = Adjust safety settings
-        // See https://ai.google.dev/gemini-api/docs/safety-settings
-    )
+    private val model = GeminiModelHelper.Build()
+        .setModel(GeminiModelHelper.ModelType.FLASH)
+        .setApiKey(BuildConfig.gemini_api_key)
+        .setResponseType(GeminiModelHelper.ResponseType.JSON)
+        .build()
 
     fun updateSelectedImageUri(uri: Uri) {
         _uiStateFlow.update { it.copy(selectedImageUri = uri) }
@@ -61,7 +52,7 @@ class MainViewModel : ViewModel() {
         val response = chat.sendMessage(
             content {
                 image(image)
-                text("添付したチラシに載っている食材を3つ以上利用した、栄養バランスの取れた献立を考えてください\nただし献立は以下の要件を満たしてください\n- 献立のなかで合計3つ以上の食材がこのチラシから参照されていれば大丈夫です\n- 調味料に関してはチラシの内容を考慮する必要はありません。全ての調味料がすでに揃っている前提で献立を考えてください\n- チラシの中で特に安くなっているものを優先して利用してください")
+                text("添付したチラシに載っている食材を3つ以上利用した、栄養バランスの取れた献立を考えてください\nただし献立は以下の要件を満たしてください\n- 献立のなかで合計3つ以上の食材がこのチラシから参照すること\n- 調味料に関してはチラシの内容を考慮する必要はありません。全ての調味料がすでに揃っている前提で献立を考えてください\n- チラシの中で特に安くなっているものを優先して利用してください")
             }
         )
 
