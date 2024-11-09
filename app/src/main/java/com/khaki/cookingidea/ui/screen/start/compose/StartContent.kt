53f144be07de8617ccd26676790c2bf5ce35c68e
Khaki <ponpontanuki16@gmail.com>
2024-11-09 19:48:03 +0900

    様々なデバイスに対応できるよう修正

--- app/src/main/java/com/khaki/cookingidea/ui/screen/start/compose/StartContent.kt
+++ app/src/main/java/com/khaki/cookingidea/ui/screen/start/compose/StartContent.kt
@@ -9,7 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -18,7 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import com.khaki.cookingidea.R
 import com.khaki.cookingidea.ui.theme.CookingIdeaTheme
@@ -36,13 +39,14 @@ fun StartContent(
                 color = MaterialTheme.colorScheme.primary
             )
             .padding(
-                top = 64.dp,
                 start = 32.dp,
                 end = 32.dp,
-                bottom = 64.dp,
-            ),
+            )
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+
+        Spacer(modifier = Modifier.height(64.dp))
 
         Text(
             text = "献立決まるくん",
@@ -54,6 +58,10 @@ fun StartContent(
 
         Image(
             modifier = Modifier
+                .sizeIn(
+                    maxWidth = 320.dp,
+                    maxHeight = 320.dp,
+                )
                 .fillMaxWidth()
                 .aspectRatio(1f)
                 .clip(
@@ -66,12 +74,15 @@ fun StartContent(
         Spacer(modifier = Modifier.height(48.dp))
 
         Text(
-            text = "このアプリでは、AIの技術を利用して指定した食材、チラシなどから、おすすめの献立をサジェストします。",
+            text = "このアプリでは、AIの技術を利用して、\n指定した食材・チラシなどから、\nおすすめの献立をサジェストします。",
             style = MaterialTheme.typography.bodyLarge,
             color = MaterialTheme.colorScheme.onPrimary
         )
 
-        Spacer(modifier = Modifier.weight(1f))
+        Spacer(
+            modifier = Modifier
+                .height(36.dp)
+        )
 
         ElevatedButton(
             onClick = onClickStart
@@ -80,12 +91,14 @@ fun StartContent(
                 text = "使ってみる"
             )
         }
+
+        Spacer(modifier = Modifier.height(64.dp))
     }
 }
 
-@Preview
+@PreviewScreenSizes
 @Composable
-fun StartContentPreview() {
+private fun StartContentPreview() {
 
     CookingIdeaTheme {
         StartContent(
